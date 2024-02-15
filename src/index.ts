3f6fe19d04765f3fe154e023473f8b89a2177ec2
Sirius B <root.siriusb@gmail.com>
2024-02-15 13:57:03 +0100

    i don't even know where I copied this from

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { Injector, Logger, webpack } from "replugged";
-
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
-
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+document.body.addEventListener("keyup", (e) => {
+  if (e.key === "F9") {
+    replugged.quickCSS.reload()
   }
-}
-
-export function stop(): void {
-  inject.uninjectAll();
-}
+});
